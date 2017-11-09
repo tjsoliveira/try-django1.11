ed02fac6e7678b90aefd1a958470f2fd345c4e8c
tjsoliveira <thiagocorey@gmail.com>
2017-11-09 13:29:40 -0300

    Try Django 1.11 - 19 - Restaurant Profile Detail

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-
+from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
 from .models import RestaurantLocation
 
 class RestaurantListView(ListView):
@@ -7,7 +7,7 @@ class RestaurantListView(ListView):
     template_name = 'restaurants/restaurants_list.html'
     queryset = RestaurantLocation.objects.all()
 
-class DetailRestaurantView(ListView):
+class CategoryRestaurantView(ListView):
 
     template_name = 'restaurants/restaurants_list.html'
 
@@ -18,3 +18,14 @@ class DetailRestaurantView(ListView):
         else:
             queryset = RestaurantLocation.objects.none()
         return queryset
+
+class RestaurantDetailView(DetailView):
+
+    template_name = 'restaurants/restaurant_detail.html'
+    queryset = RestaurantLocation.objects.all()
+
+    def get_object(self, *args, **kwargs):
+        print(self.kwargs)
+        rest_id = self.kwargs.get('pk')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id)
+        return obj
