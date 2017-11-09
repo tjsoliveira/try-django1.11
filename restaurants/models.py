999625ed2deea290a574425d84306900eef820a0
tjsoliveira <thiagocorey@gmail.com>
2017-11-09 07:32:54 -0300

    Try Django 1.11 - 15 - More on Model Fields

--- restaurants/models.py
+++ restaurants/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class RestaurantLocation(models.Model):
 
     name        = models.CharField(max_length=120)
-    location    = models.CharField(blank=True, null=True, max_length=120)
+    location    = models.CharField(max_length=120, blank=True, null=True)
+    category    = models.CharField(max_length=120, blank=True, null=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
