08075456a7c62c98655d3806a92950ca7656e06f
tjsoliveira <thiagocorey@gmail.com>
2017-11-09 08:14:45 -0300

    Try Django 1.11 - 16 - Displaying Saved Data

--- restaurants/models.py
+++ restaurants/models.py
@@ -8,3 +8,6 @@ class RestaurantLocation(models.Model):
     category    = models.CharField(max_length=120, blank=True, null=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,17 +1,10 @@
-import random
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .models import RestaurantLocation
 
-class HomeView(TemplateView):
-
-    template_name = "home.html"
-
-    def get_context_data(self, *args, **kwargs):
-        #context = super(HomeView, self).get_context_data(*args, **kwargs)
-        some_list = [random.randint(0, 10000), random.randint(0, 10000), random.randint(0, 10000)]
-        context = {
-            'html_var': 'context variable',
-            'random_num': random.randint(0, 10000),
-            'some_list': some_list
-        }
-        return context
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
