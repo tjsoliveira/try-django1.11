15413e9cf594468aef9089313269277fcab5d448
tjsoliveira <thiagocorey@gmail.com>
2017-11-10 14:18:47 -0300

    Try Django 1.11 - 25 - THe Power of Django Forms

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,21 +6,22 @@ from django.http import HttpResponseRedirect
 
 def restaurant_create_view(request):
     template_name = 'restaurants/restaurant_create_form.html'
-    if request.method == "GET":
-        print("get data")
+    form = RestaurantCreateForm(request.POST or None)
+    errors = None
     if request.method == "POST":
-        print("post data")
-        print(request.POST)
-        name = request.POST.get("name")
-        location = request.POST.get("location")
-        category = request.POST.get("category")
-        obj = RestaurantLocation.objects.create(
-            name = name,
-            location = location,
-            category = category
-        )
-        return HttpResponseRedirect("/restaurants/")
-    context = {}
+        if form.is_valid():
+            obj = RestaurantLocation.objects.create(
+                name = form.cleaned_data.get('name'),
+                location = form.cleaned_data.get('location'),
+                category = form.cleaned_data.get('category')
+            )
+            return HttpResponseRedirect("/restaurants/")
+        if form.errors:
+            errors = form.errors
+    context = {
+        'form':form,
+        'errors': errors
+    }
     return render(request, template_name, context)
 
 class RestaurantListView(ListView):
