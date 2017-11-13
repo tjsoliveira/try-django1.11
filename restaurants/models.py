1d382849375d6753a46de58daddea135b9285e5c
tjsoliveira <thiagocorey@gmail.com>
2017-11-13 09:21:21 -0300

    Try-Django-1.11 - 28 - Letting Users Own Data

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,12 +1,16 @@
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 
 from .utils import unique_slug_generator
 from .validators import validate_category
 
+User = settings.AUTH_USER_MODEL
+
 # Create your models here.
 class RestaurantLocation(models.Model):
 
+    owner       = models.ForeignKey(User)
     name        = models.CharField(max_length=120)
     location    = models.CharField(max_length=120, blank=True, null=True)
     category    = models.CharField(max_length=120, blank=True, null=True, validators=[validate_category])
